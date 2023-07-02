99bac1c3750e4f8cf272ba731d1f29ee2e906a90
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-07-02 19:10:26 +0530

    Initial commit

---
+++ 🏠_Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+st.title("Diabetes Disease Classification")
+
+
+want_to_contribute = st.button("Click Here to upload the Image")
+if want_to_contribute:
+    switch_page("Upload Image")
