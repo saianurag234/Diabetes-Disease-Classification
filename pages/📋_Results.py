0e8965a2106d36c77fe724ac28eb7eb903c56196
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-07-02 22:30:04 +0530

    Update 📋_Results.py

--- pages/📋_Results.py
+++ pages/📋_Results.py
@@ -3,6 +3,8 @@ from streamlit_extras.switch_page_button import switch_page
 import numpy as np
 import time
 
+image_url = "https://img.freepik.com/free-vector/diabetes-flat-composition-medical-with-patient-symptoms-complications-blood-sugar-meter-treatments-medication_1284-28998.jpg?w=500"
+
 if 'prediction' in st.session_state:
     final_prediction = st.session_state['prediction']
 
@@ -11,6 +13,7 @@ if(final_prediction == 0):
 
 if(final_prediction == 1):
     st.markdown("<h1 style='font-size: 38px;'>The Patient is having Diabetes</h1>", unsafe_allow_html=True)
+    st.image(image_url)
 
 back = st.button("Click here to try again")
 if back:
