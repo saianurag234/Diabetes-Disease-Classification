afef66784408c731e74d683fb0b4be6ba7bdbf65
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-07-02 22:36:21 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -3,11 +3,17 @@ from streamlit_extras.switch_page_button import switch_page
 
 image_url = "https://assets.newatlas.com/dims4/default/3461759/2147483647/strip/true/crop/7360x4907+0+3/resize/840x560!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2F10%2F7f%2F5e48f79245c0b831a58d7cf8fb1d%2Fdepositphotos-228244172-xl.jpg"
 
-#st.title("Diabetes Disease Classification")
-st.markdown("<h1 style='font-size: 48px; text-align: left;'>Diabetes Disease Classification</h1>", unsafe_allow_html=True)
+st.title("Diabetes Disease Classification")
 st.title(" ")
 
-st.image(image_url,width=500)
+#st.image(image_url,width=500)
+
+st.markdown(
+    f'<div style="display: flex; justify-content: center;">'
+    f'<img src="{image_url}" alt="Image" width="500" align="left">'
+    '</div>',
+    unsafe_allow_html=True
+)
 
 st.title(" ")
 data_entry = st.button("Click Here to enter the details of the patients")
